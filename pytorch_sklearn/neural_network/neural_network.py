- module parameter getter (useful for e.g. gradient clipping) - LossPlot(ter) rename - improved Verbose and print_progress - some bug fixes

pytorch_sklearn/neural_network/neural_network.py
@@ -283,7 +283,7 @@ class NeuralNetwork:
     ):
         # Handle None inputs.
         batch_size = batch_size if batch_size is not None else (self._batch_size if self._batch_size is not None else 32)
-        use_cuda = use_cuda if use_cuda is not None else (self._use_cuda if self._use_cuda is not None else True)
+        use_cuda = use_cuda if use_cuda is not None else (self._use_cuda if self._use_cuda is not None else torch.cuda.is_available())
         fits_gpu = fits_gpu if fits_gpu is not None else (self._fits_gpu if self._fits_gpu is not None else False)
 
         device = "cuda" if use_cuda else "cpu"
@@ -321,7 +321,7 @@ class NeuralNetwork:
     ):
         # Handle None inputs.
         batch_size = batch_size if batch_size is not None else (self._batch_size if self._batch_size is not None else 32)
-        use_cuda = use_cuda if use_cuda is not None else (self._use_cuda if self._use_cuda is not None else True)
+        use_cuda = use_cuda if use_cuda is not None else (self._use_cuda if self._use_cuda is not None else torch.cuda.is_available())
         fits_gpu = fits_gpu if fits_gpu is not None else (self._fits_gpu if self._fits_gpu is not None else False)
 
         device = "cuda" if use_cuda else "cpu"
@@ -387,7 +387,7 @@ class NeuralNetwork:
     ):
         # Handle None inputs.
         batch_size = batch_size if batch_size is not None else (self._batch_size if self._batch_size is not None else 32)
-        use_cuda = use_cuda if use_cuda is not None else (self._use_cuda if self._use_cuda is not None else True)
+        use_cuda = use_cuda if use_cuda is not None else (self._use_cuda if self._use_cuda is not None else torch.cuda.is_available())
         fits_gpu = fits_gpu if fits_gpu is not None else (self._fits_gpu if self._fits_gpu is not None else False)
 
         device = "cuda" if use_cuda else "cpu"
@@ -504,14 +504,20 @@ class NeuralNetwork:
         self._original_state_dict = None
 
     def get_module_weights(self):
+        ''' Returns the state dict of all the modules that are part of this network. '''
         return self.module.state_dict()
     
     def load_module_weights(self, state_dict):
+        ''' Loads the given state dict into the modules that are part of this network. '''
         # Workaround for optimizer being on the wrong device. Check ``func_utils.optimizer_to`` for more info.
         checkpoint_device = state_dict[next(iter(state_dict))].device
         self.to_device(checkpoint_device)
 
         self.module.load_state_dict(state_dict, strict=False)
+
+    def get_module_parameters(self):
+        ''' Returns all the parameters of the modules that are part of this network. '''
+        yield from self.module.parameters()
 
     def state_dict(self):
         return {

pytorch_sklearn/utils/model_utils.py
@@ -170,11 +170,7 @@ def plot_receptive_fields(*models: torch.nn.Module, x: torch.Tensor, **kwargs):
         ax = axs[i // cols, i % cols]
         ax.imshow(rf[0].permute(1, 2, 0).cpu(), interpolation='none')
         ax.set_title(type(model).__name__)
-
-    fig.tight_layout()
-    plt.subplots_adjust(wspace=-0.55, hspace=0.15)
-    plt.show()
-
+    return fig, axs
 
 
 def find_optimal_grid_size(N, aspect_ratio: Union[int, tuple] = 1, fit_rect_if_above=None):